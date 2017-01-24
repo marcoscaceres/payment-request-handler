8ebd4550e84133b3effc21c33af4f4a2ea162101
Marcos Caceres <marcos@marcosc.com>
2017-01-24 18:15:04 +1100

    fix: add and call paymentIsProcessed() (closes #5)

--- examples/sw_onpaymentrequest.js
+++ examples/sw_onpaymentrequest.js
@@ -3,12 +3,16 @@ const paymentsUI = new URL("/pay-for-something", location).href;
 
 // Fires when the merchant calls PaymentRequest.show()
 addEventListener("paymentrequest", ev => {
-  ev.waitUntil(async() => {
-    const { request } = ev;
-    // browser provided payment flow window
-    const client;
-    // Handle the merchant calling PaymentRequest.abort()
-    ev.request.onabort = async(ev) => {
+  ev.waitUntil(paymentIsProcessed(ev));
+});
+
+// Attempt to process the request for payment
+async function paymentIsProcessed(ev){
+  const { request } = ev;
+  // browser provided payment flow window
+  const client;
+  // Handle the merchant calling PaymentRequest.abort()
+  ev.request.onabort = async(ev) => {
       if (!client) {
         return;
       }
@@ -17,39 +21,38 @@ addEventListener("paymentrequest", ev => {
     }
     // Security check, but without leaking URL information to payment
     // processor.
-    const isAllowed = await verifyDomain(ev.domain);
-    if (!isAllowed) {
-      const msg = `The domain ${ev.domain} is not allowed to request payment!`;
-      throw new DOMException(msg, "SecurityError");
-    }
-    const id = request.paymentRequestID || generateId();
-    if (request.requestedFields.length) {
-      // Gather email, name, phone, shipping address
-      const fields = await lookupUserInfo(request.requestedFields);
-    }
-    const creditCardDetails = await generateCreditCard();
-    const init = Object.assign({ fields }, { details: creditCardDetails });
-    // PaymentResponse can now be constructed and returned.
-    const response = new PaymentResponse(id, "basic-card#visa", init);
-    ev.respondWith(response);
-    try {
-      client = await ev.openClientWindow(paymentsUI);
-      const msg = {
-        action: "showItems",
-        displayItems: request.displayItems,
-        total: request.total,
-      };
-      await coordiantePurchaseWithUser(client, response);
-    } catch (err) {
-      console.error(err);
-      await response.complete("fail");
-    }
-    // The payment flow ends once payment is compete
-    const howDidItComplete = await response.isComplete;
-    // Payment handlers can track if payments are succeeding per origin
-    analytics(ev.origin, howDidItComplete);
-  });
-});
+  const isAllowed = await verifyDomain(ev.domain);
+  if (!isAllowed) {
+    const msg = `The domain ${ev.domain} is not allowed to request payment!`;
+    throw new DOMException(msg, "SecurityError");
+  }
+  const id = request.paymentRequestID || generateId();
+  if (request.requestedFields.length) {
+    // Gather email, name, phone, shipping address
+    const fields = await lookupUserInfo(request.requestedFields);
+  }
+  const creditCardDetails = await generateCreditCard();
+  const init = Object.assign({ fields }, { details: creditCardDetails });
+  // PaymentResponse can now be constructed and returned.
+  const response = new PaymentResponse(id, "basic-card#visa", init);
+  ev.respondWith(response);
+  try {
+    client = await ev.openClientWindow(paymentsUI);
+    const msg = {
+      action: "showItems",
+      displayItems: request.displayItems,
+      total: request.total,
+    };
+    await coordiantePurchaseWithUser(client, response);
+  } catch (err) {
+    console.error(err);
+    await response.complete("fail");
+  }
+  // The payment flow ends once payment is compete
+  const howDidItComplete = await response.isComplete;
+  // Payment handlers can track if payments are succeeding per origin
+  analytics(ev.origin, howDidItComplete);
+}
 
 async function coordiantePurchaseWithUser(client, response, msg) {
   const result = await promisedPostMessage(client, msg);
