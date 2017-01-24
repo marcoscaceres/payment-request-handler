dbf26dd36b26f84a0c95c9e573193598b14a453f
Marcos Caceres <marcos@marcosc.com>
2017-01-24 21:17:54 +1100

    fix: rewrite respondWith() (closes #7)

--- examples/sw_onpaymentrequest.js
+++ examples/sw_onpaymentrequest.js
@@ -1,99 +1,96 @@
-// User interface for payment flow:
+"use strict";
+// User interface for payment flow - we could also pull this from Cache API
 const paymentsUI = new URL("/pay-for-something", location).href;
 
 // Fires when the merchant calls PaymentRequest.show()
 addEventListener("paymentrequest", ev => {
-  ev.waitUntil(paymentIsProcessed(ev));
+  ev.respondWith(createResponseAndProcessPayment(ev));
 });
 
 // Attempt to process the request for payment
-async function paymentIsProcessed(ev){
-  const { request } = ev;
-  // browser provided payment flow window
-  const client;
-  // Handle the merchant calling PaymentRequest.abort()
-  ev.request.onabort = async(ev) => {
-      if (!client) {
-        return;
-      }
-      // should call window.close();
-      await promisedPostMessage(client, { action: "close" });
-    }
-    // Security check, but without leaking URL information to payment
-    // processor.
+async function createResponseAndProcessPayment(ev) {
+  const {request, paymentRequestID} = ev;
+  const responseId = paymentRequestID || generateId();
+  // Security check, but without leaking URL information to payment
+  // processor.
   const isAllowed = await verifyDomain(ev.domain);
   if (!isAllowed) {
-    const msg = `The domain ${ev.domain} is not allowed to request payment!`;
-    throw new DOMException(msg, "SecurityError");
+    console.error(`The domain ${ev.domain} is not allowed to request payment!`);
+    const response = new sPaymentResponse(responseId);
+    await response.complete("fail");
+    return response;
   }
-  const id = request.paymentRequestID || generateId();
+  // Gather email, name, phone, shipping address, etc. if requested
+  const fields = {};
   if (request.requestedFields.length) {
-    // Gather email, name, phone, shipping address
-    const fields = await lookupUserInfo(request.requestedFields);
+    Object.assign(fields, await lookupUserInfo(request.requestedFields));
   }
   const creditCardDetails = await generateCreditCard();
-  const init = Object.assign({ fields }, { details: creditCardDetails });
+  const init = Object.assign({}, {fields}, {details: creditCardDetails});
   // PaymentResponse can now be constructed and returned.
-  const response = new PaymentResponse(id, "basic-card#visa", init);
-  ev.respondWith(response);
+  const response = new PaymentResponse(responseId, "basic-card#visa", init);
+  ev.waitUntil(coordiantePurchaseWithUser(ev, response));
+  return response;
+}
+
+// Handle back and forth between end-user and merchant
+async function coordiantePurchaseWithUser(ev, response) {
+  const {request} = ev;
+  // Handle the merchant calling PaymentRequest.abort()
+  request.onabort = async() => {
+    if (!client) {
+      return;
+    }
+    // should call window.close();
+    await promisedPostMessage(client, {action: "close"});
+  };
+  const client = await ev.openClientWindow(paymentsUI);
+  // browser provided payment-flow window (overlay, sheet, whatever)
+  const msg = {
+    action: "showItems", // Let's see if the user wants to pay for these things
+    displayItems: request.displayItems,
+    total: request.total,
+  };
+  const result = await promisedPostMessage(client, msg);
   try {
-    client = await ev.openClientWindow(paymentsUI);
-    const msg = {
-      action: "showItems",
-      displayItems: request.displayItems,
-      total: request.total,
-    };
-    await coordiantePurchaseWithUser(client, response);
+    await coordiateCommuncations(result, response, client);
   } catch (err) {
-    console.error(err);
-    await response.complete("fail");
+    response.compete("fail");
   }
   // The payment flow ends once payment is compete
   const howDidItComplete = await response.isComplete;
   // Payment handlers can track if payments are succeeding per origin
   analytics(ev.origin, howDidItComplete);
-}
-
-async function coordiantePurchaseWithUser(client, response, msg) {
-  const result = await promisedPostMessage(client, msg);
-  switch (result.action) {
-    case "updateShipping":
-      const details = await response.updateShippingAddress(result.newShippingAddress);
-      if (details) {
-        // rinse and repeat until consensus is reached...
-        const msg = Object.assign({ action: "invalidShippingAddress" }, details);
-        await coordiantePurchaseWithUser(client, response, msg);
-      }
-      break;
-    case "updateShippingOption":
-      const details = await response.updateShippingOption(result.newShippingOptionId);
-      if (details) {
-        // rinse and repeat until consensus is reached...
-        const msg = Object.assign({ action: "invalidShippingOption" }, details);
-        await coordiantePurchaseWithUser(client, response, msg)
-      }
-      break;
-    case "abort":
-      throw new Error("User doesn't want this!");
-    case "buy it":
-      break;
-    default:
-      throw new Error("Unknown action");
-  }
   return;
 }
 
-function promisedPostMessage(client, msg) {
-  return new Promise(resolve => {
-    const msgId = String(Math.random()).substr(2);
-    const uniqueMsg = Object.assign({ msgId }, msg);
-    client.addEventListener("message", function msgHandler({ data }) {
-      if (data.msgId !== msgId) {
-        return; // not for me!
-      }
-      client.removeEventListener("message", msgHandler);
-      resolve(data);
-    });
-    client.postMessage(uniqueMsg);
-  });
+async function coordiateCommuncations(result, response, client) {
+  let msg;
+  switch (result.action) {
+  case "buy-it":
+    break; // Awesome, we are done!
+  case "updateShipping": {
+    const merchantResult = await response.updateShippingAddress(result.newShippingAddress);
+    if (merchantResult) {
+      msg = Object.assign({action: "invalidShippingAddress"}, merchantResult);
+    }
+    break;
+  }
+  case "updateShippingOption": {
+    const merchantResult = await response.updateShippingOption(result.newShippingOptionId);
+    if (merchantResult) {
+      msg = Object.assign({action: "invalidShippingOption"}, merchantResult);
+    }
+    break;
+  }
+  case "abort":
+    throw new Error("User doesn't want this!");
+  default:
+    throw new Error("Unknown action");
+  }
+  if (msg) {
+    // recursively repeat until the both parties and user says "buy-it".
+    const endUserSays = await promisedPostMessage(client, msg);
+    await coordiateCommuncations(endUserSays, response, client);
+  }
 }
